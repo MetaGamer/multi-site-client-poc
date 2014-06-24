be7977898b94180ff4af21f3c29edd2ee8c61fc3
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-23 17:09:23 -0700

    Add license info

--- app/features/sessions/data.req.js
+++ app/features/sessions/data.req.js
@@ -48,3 +48,25 @@ hub.on('user:logout', session.destroy, session)
 hub.on('user:check', session.check, session)
 
 module.exports = session
+
+//==========================================================================//
+// This file is part of multi-site-client-poc.                              //
+//                                                                          //
+// multi-site-client-poc is Copyright 2014 Volary Foundation and            //
+// Contributors                                                             //
+//                                                                          //
+// multi-site-client-poc is free software: you can redistribute it and/or   //
+// modify it under the terms of the GNU Affero General Public License as    //
+// published by the Free Software Foundation, either version 3 of the       //
+// License, or at your option) any later version.                           //
+//                                                                          //
+// multi-site-client-poc is distributed in the hope that it will be useful, //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero  //
+// General Public License for more details.                                 //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public         //
+// License along with multi-site-client-poc.  If not, see                   //
+// <http://www.gnu.org/licenses/>.                                          //
+//==========================================================================//
+
